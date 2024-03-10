b83add3899df92ad7d0ff3d042ed202569a1cd3e
jimmy wilk <mafsjimmy@gmail.com>
2024-03-10 16:18:59 +0530

    inital commit

---
+++ Color Flipper/colorflipper.js
@@ -0,0 +1,45 @@
+// document.addEventListener("input",()=>{
+// var colorName = document.querySelector("#input").value
+// var bodybg = document.querySelector(".body")
+// bodybg.style.background = colorName}
+// )
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     var inputElement = document.querySelector("#input");
+
+//     inputElement.addEventListener("input", function () {
+//         var colorName = inputElement.value.toLowerCase();
+//         var bodybg = document.querySelector(".body");
+//         bodybg.style.background = colorName;
+//     });
+// });
+
+
+    // document.querySelector("#input").addEventListener("input", () => {
+    //   document.querySelector(".body").style.background = document.querySelector("#input").value.toLowerCase();
+    // });
+
+
+    document.querySelector(".input").addEventListener("click", () =>{
+    document.querySelector(".body").style.background = document.querySelector("#input").value.toLowerCase()
+    document.querySelector("#input").value=""})
+
+    
+    // document.querySelector(".input").addEventListener("click", () => {
+    //     document.querySelector(".body").style.background = document.querySelector("#input").value.toLowerCase();
+    //     document.querySelector("#input").value = "";
+    // });
+    
+
+
+
+// document.querySelector("#input").addEventListener("input", (e) => document.body.style.background = e.target.value.toLowerCase());
+
+// const { value } = document.querySelector("#input");
+// document.querySelector(".body").style.background = value.toLowerCase();
+
+// const textContent = document.querySelector("#input").textContent;
+// document.body.style.background = textContent.toLowerCase();
+
+
+
