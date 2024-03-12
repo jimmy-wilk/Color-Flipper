c94b38e65c21f368cc71a940b9404d9c0e92ec01
jimmy wilk <mafsjimmy@gmail.com>
2024-03-12 08:50:16 +0530

    working on count project 2

---
+++ Count/count.js
@@ -0,0 +1,41 @@
+let innerdigit = document.querySelector(".digit").innerHTML = 0
+// innerdigit = 0
+let decrease = document.querySelector(".dec")
+let reset = document.querySelector(".reset")
+let increase = document.querySelector(".inc")
+decrease.addEventListener("click",()=>{
+    innerdigit--
+})
+reset.addEventListener("click",()=>{
+    innerdigit = 0
+})
+increase.addEventListener("click",()=>{
+    innerdigit++
+})
+
+
+
+// let innerdigit = document.querySelector(".digit");
+// let decrease = document.querySelector(".dec");
+// let reset = document.querySelector(".reset");
+// let increase = document.querySelector(".inc");
+
+
+// // Initial value
+// let count = 0;
+// innerdigit.innerHTML = count;
+
+// decrease.addEventListener("click", () => {
+//     count--;
+//     innerdigit.innerHTML = count;
+// });
+
+// reset.addEventListener("click", () => {
+//     count = 0;
+//     innerdigit.innerHTML = count;
+// });
+
+// increase.addEventListener("click", () => {
+//     count++;
+//     innerdigit.innerHTML = count;
+// });
